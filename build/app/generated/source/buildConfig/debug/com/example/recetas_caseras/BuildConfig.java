49a94dfe21fb63a1fcb2c666592579f82b2715b3
GustavoGutierrez98 <114888877+GustavoGutierrez98@users.noreply.github.com>
2023-06-01 12:58:41 -0600

    Add files via upload

---
+++ build/app/generated/source/buildConfig/debug/com/example/recetas_caseras/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.recetas_caseras;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.recetas_caseras";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.0";
+}
